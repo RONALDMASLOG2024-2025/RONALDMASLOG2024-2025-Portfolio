8346c4e395fdb01f1ef675a06d8d1ed63a025845
maslog <ronaldmaslog5@gmail.com>
2024-07-31 06:49:15 +0800

    Adding the config for portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RONALDMASLOG2024-2025.github.io.git/"
+  base: "/RONALDMASLOG2024-2025-Portfolio.git/"
 });
